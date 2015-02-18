6e0c45b255fcbb9f92bb76bbd14e18a7a94a9946
Diego Carciente <diego.carciente@gmail.com>
2015-02-18 15:38:01 -0430

    agregando latitud y longitud

--- www/js/empresas/informatica.js
+++ www/js/empresas/informatica.js
@@ -8,7 +8,8 @@ var informatica = [
             direccion  :  'En en el fin del mundo',
             correo     :  'key@io.com',
             url        :  'www.keySystems1.com',
-            position   :  { latitude: 10.5,longitude: -66.91667 }
+            position     :  { latitude: 10.375725, longitude:  -66.955842 },
+            positionmap  :  { lat: 10.375725, lng:  -66.955842 }
         }, {
             categoriaId:  0,
             empresaId  :  1,
@@ -18,6 +19,7 @@ var informatica = [
             direccion  :  'En en el fin del mundo',
             correo     :  'Arepa@io.com',
             url        :  'www.ArepaSotf1.com',
-            position   :  { latitude: 10.1585125,longitude: -67.99737529999999 }
+            position     :  { latitude: 10.375725, longitude:  -66.955842 },
+            positionmap  :  { lat: 10.375725, lng:  -66.955842 }
         }
     ]
