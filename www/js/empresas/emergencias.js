7a3e93d66bbbbf2f6d09aaa31b9591c0980425af
Diego Carciente <diego.carciente@gmail.com>
2015-02-23 23:42:37 -0430

    Cambiando el archivo informatica.js por emergencias.js agregando imagenes para empresa detalle falta arreglar el estilo de la vista

--- www/js/empresas/emergencias.js
+++ www/js/empresas/emergencias.js
@@ -1,26 +1,26 @@
-var informatica = [
+var emergencias = [
         {   
             estadoId     :  0,
             categoriaId  :  0,
             empresaId    :  0,
-            nombre       :  'key Systems1 Estado 0',
-            icon         :  'img/ionic.png',
-            tel          :  '0212 - 329 00 87',
-            direccion    :  'En en el fin del mundo',
-            correo       :  'key@io.com',
-            url          :  'www.keySystems1.com',
+            nombre       :  'Electricidad de Caraca',
+            icon         :  'img/categorias/alert11.png',
+            tel          :  '(0212) 502-2111 / 207-5811',
+            direccion    :  '',
+            correo       :  '',
+            url          :  '',
             position     :  { latitude: 10.375725, longitude:  -66.955842 },
             positionmap  :  { lat: 10.375725, lng:  -66.955842 }
         }, {
-            estadoId     :  1,
+            estadoId     :  0,
             categoriaId  :  0,
             empresaId    :  1,
-            nombre       :  'ArepaSotf1 Estado 1',
-            icon         :  'img/ionic.png',
-            tel          :  '0212 - 329 00 87',
-            direccion    :  'En en el fin del mundo',
-            correo       :  'Arepa@io.com',
-            url          :  'www.ArepaSotf1.com',
+            nombre       :  'Corpoven',
+            icon         :  'img/categorias/alert11.png',
+            tel          :  '(0212) 708.13.14 - 708.13.15',
+            direccion    :  '',
+            correo       :  '',
+            url          :  '',
             position     :  { latitude: 10.375725, longitude:  -66.955842 },
             positionmap  :  { lat: 10.375725, lng:  -66.955842 }
         }, {
@@ -28,7 +28,7 @@ var informatica = [
             categoriaId  :  0,
             empresaId    :  1,
             nombre       :  'ArepaSotf1 Estado 0',
-            icon         :  'img/ionic.png',
+            icon         :  'img/categorias/alert11.png',
             tel          :  '0212 - 329 00 87',
             direccion    :  'En en el fin del mundo',
             correo       :  'Arepa@io.com',
@@ -36,11 +36,11 @@ var informatica = [
             position     :  { latitude: 10.375725, longitude:  -66.955842 },
             positionmap  :  { lat: 10.375725, lng:  -66.955842 }
         }, {
-            estadoId     :  2,
+            estadoId     :  0,
             categoriaId  :  0,
             empresaId    :  1,
             nombre       :  'ArepaSotf1 Estado 2',
-            icon         :  'img/ionic.png',
+            icon         :  'img/categorias/alert11.png',
             tel          :  '0212 - 329 00 87',
             direccion    :  'En en el fin del mundo',
             correo       :  'Arepa@io.com',
