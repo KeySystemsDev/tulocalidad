d9cbb32d1a6bf58b245071daea50d03fc922b226
Diego Carciente <diego.carciente@gmail.com>
2015-02-24 13:16:11 -0430

    modificando las publicidades

--- www/js/publicidad.js
+++ www/js/publicidad.js
@@ -2,102 +2,102 @@ var publicidad = [{
         id : 0, 
         empresa: 'Key systems',
         href: '#/empresas-detalle/0/0',
-        image: 'img/m-1.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/zanos.jpg',
+        estadoId: 0
     }, {
         id : 1,
     	  empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/p-1.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/p-1.jpg',
+        estadoId: 0
     }, {
        	id : 2,
        	empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/p-2.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/p-2.jpg',
+        estadoId: 0
     }, {
        	id : 3,
        	empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/p-3.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/p-3.jpg',
+        estadoId: 0
     }, {
        	id : 4,
        	empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/p-4.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/p-4.jpg',
+        estadoId: 0
     }, {
         id : 5,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/l-1.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/keysystems.jpg',
+        estadoId: 0
     }, {
         id : 6,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/l-2.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/mixbox.jpg',
+        estadoId: 0
     }, {
         id : 7,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/m-2.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/m-2.jpg',
+        estadoId: 0
     }, {
         id : 8,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/m-3.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/m-3.jpg',
+        estadoId: 0
     }, {
         id : 9,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/p-5.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/p-5.jpg',
+        estadoId: 0
     }, {
         id : 10,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/p-6.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/p-6.jpg',
+        estadoId: 0
     }, {
         id : 11,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/p-7.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/p-7.jpg',
+        estadoId: 0
     }, {
         id : 12,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/p-8.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/p-8.jpg',
+        estadoId: 0
     }, {
         id : 13,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/m-4.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/m-4.jpg',
+        estadoId: 0
     }, {
         id : 14,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/m-5.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/m-5.jpg',
+        estadoId: 0
     }, {
         id : 15,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/l-3.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/l-3.jpg',
+        estadoId: 0
     }, {
         id : 16,
         empresa: 'Key systems',
         href:'#/empresas-detalle/0/0',
-        image: 'img/l-4.jpg',
-        mpresaId: 0
+        image: 'img/publicidad/l-4.jpg',
+        estadoId: 0
     }]
