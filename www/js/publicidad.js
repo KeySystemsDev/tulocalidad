1cb09a809588cb3ad106a77d7f5a12c4aeb1b429
Diego Carciente <diego.carciente@gmail.com>
2015-02-24 22:38:48 -0430

    Modificando la estructura del arreglo json de la publicidad

--- www/js/publicidad.js
+++ www/js/publicidad.js
@@ -1,25 +1,6 @@
-var publicidad = [{
-        id : 0,
-        empresa: 'Key systems',
-        href:'#/empresas-detalle/0/0',
-        image: 'img/publicidad/keysystems.jpg',
-        estadoId: 0
-    }, {
-        id : 1,
-        empresa: 'Key systems',
-        href:'#/empresas-detalle/0/0',
-        image: 'img/publicidad/mixbox.jpg',
-        estadoId: 0
-    }, {
-        id : 3,
-        empresa: 'Key systems',
-        href:'#/empresas-detalle/0/0',
-        image: 'img/publicidad/zanos.jpg',
-        estadoId: 0
-    }, {
-        id : 4,
-        empresa: 'Key systems',
-        href:'#/empresas-detalle/0/0',
-        image: 'img/publicidad/tupintor.jpg',
-        estadoId: 0
-    }]
+var publicidad = [
+		{"id ":0,"empresa":"Key Systems","href":"#/empresas-detalle/0/0","image":"img/publicidad/keysystems.jpg","estadoId":0},
+		{"id ":1,"empresa":"Mix Box","href":"#/empresas-detalle/0/0","image":"img/publicidad/mixbox.jpg","estadoId":0},
+		{"id ":2,"empresa":"Zanos","href":"#/empresas-detalle/0/0","image":"img/publicidad/zanos.jpg","estadoId":0},
+		{"id ":3,"empresa":"Tu Pintor","href":"#/empresas-detalle/0/0","image":"img/publicidad/tupintor.jpg","estadoId":0}
+	]
