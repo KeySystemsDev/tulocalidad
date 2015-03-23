f2457776599d6f96e565c822d45e22224ae08808
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-03-23 11:31:49 -0430

    rutas de las consultas

--- www/js/services.js
+++ www/js/services.js
@@ -15,7 +15,7 @@ angular.module('starter.services', [])
 })
 
 .factory("estados", function ($resource) {
-    return $resource("http://keysystems.com.ve/tulocalidad/estados.php", //la url donde queremos consumir
+    return $resource("http://keysystems.com.ve/tulocalidad/registrar/estados.php", //la url donde queremos consumir
         {}, //aquí podemos pasar variables que queramos pasar a la consulta
         //a la función get le decimos el método, y, si es un array lo que devuelve
         //ponemos isArray en true
@@ -24,7 +24,7 @@ angular.module('starter.services', [])
 })
 
 .factory("categoria_estado", function ($resource) {
-    return $resource("http://keysystems.com.ve/tulocalidad/categoria_estado.php", //la url donde queremos consumir
+    return $resource("http://keysystems.com.ve/tulocalidad/registrar/categoria_estado.php", //la url donde queremos consumir
         {}, //aquí podemos pasar variables que queramos pasar a la consulta
         //a la función get le decimos el método, y, si es un array lo que devuelve
         //ponemos isArray en true
@@ -33,7 +33,7 @@ angular.module('starter.services', [])
 })
 
 .factory("empresas_categorias", function ($resource) {
-    return $resource("http://keysystems.com.ve/tulocalidad/empresas_categorias.php", //la url donde queremos consumir
+    return $resource("http://keysystems.com.ve/tulocalidad/registrar/empresas_categorias.php", //la registrar/url donde queremos consumir
         {}, //aquí podemos pasar variables que queramos pasar a la consulta
         //a la función get le decimos el método, y, si es un array lo que devuelve
         //ponemos isArray en true
@@ -42,7 +42,7 @@ angular.module('starter.services', [])
 })
 
 .factory("detalle_empresa", function ($resource) {
-    return $resource("http://keysystems.com.ve/tulocalidad/detalle_empresa.php", //la url donde queremos consumir
+    return $resource("http://keysystems.com.ve/tulocalidad/registrar/detalle_empresa.php", //la url donde queremos consumir
         {}, //aquí podemos pasar variables que queramos pasar a la consulta
         //a la función get le decimos el método, y, si es un array lo que devuelve
         //ponemos isArray en true
