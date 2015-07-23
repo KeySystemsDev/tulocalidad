2566f32539ff1ac67aea4a9e226ff1d04ce86b6a
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-07-22 19:35:34 -0430

    funtion recargar para la publicidad

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,13 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers','starter.services','uiGmapgoogle-maps','ngResource', 'ngCordova'])
+angular.module('starter', [ 'ionic', 
+                            'starter.controllers', 
+                            'starter.services',
+                            'uiGmapgoogle-maps',
+                            'ngResource', 
+                            'ngCordova', 
+                            'chieffancypants.loadingBar'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,7 +26,10 @@ angular.module('starter', ['ionic', 'starter.controllers','starter.services','ui
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, cfpLoadingBarProvider) {
+  
+  cfpLoadingBarProvider.includeSpinner = true;
+
   $stateProvider
 
   .state('app', {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,7 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('tulocalidadCtrl', function($scope, $rootScope, $state, $window, $ionicModal, $cordovaGeolocation, publicidad, estados, MyService) {
+.controller('tulocalidadCtrl', function($scope, $http, $rootScope, $state, $window, $ionicModal, $cordovaGeolocation, publicidad, estados, MyService) {
 
     var posOptions = {timeout: 10000, enableHighAccuracy: true};
     
@@ -66,6 +66,16 @@ angular.module('starter.controllers', [])
     }
 
     $scope.publicidades = publicidad.get({'id_estado': localStorage.getItem('id_estado')});
+
+    $scope.RecargarPublicidad = function(){
+        $http.get('http://www.tulocalidad.com.ve/movil/empresa/publicidad', {id_estado: localStorage.getItem('id_estado')})
+            .success(function(publicidad) {
+                $scope.publicidades = publicidad;
+            })
+            .finally(function() {
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+    }
 
     $scope.id_empresa = function(id_empresa) {
         MyService.id_empresa = id_empresa;
